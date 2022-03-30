5dfc2ea9e0ba5ec65f24fa755cd91f87769ae491
xsvest08 <61969702+xsvest08@users.noreply.github.com>
2022-03-30 20:22:56 +0200

    Add files via upload

---
+++ fylo.R
@@ -0,0 +1,21 @@
+#fylogenetika 
+## knihovny 
+
+##tidyverse - nejvíce z toho ggplot2 pro vizualizaci, 
+  #dìlali jsme ve škole 
+library("tidyverse")
+
+##biostrings - porovnání sekvencí, manipulace se sekvencemi
+library("Biostrings")
+
+##msa - jednotné rozhraní pro vícenásobné zarovnání 
+#https://bioconductor.org/packages/release/bioc/html/msa.html
+library("msa")
+
+##usedist - ve zkratce by mìl poèítat vzdálenosti mezi shluky (pøíbuznost)
+#https://cran.r-project.org/web/packages/usedist/usedist.pdf
+library("usedist")
+
+##ape-provádí fylogenetickou analýzu, finální knihovna 
+#http://ape-package.ird.fr/
+library("ape")
